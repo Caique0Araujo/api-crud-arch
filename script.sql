f7554ed5d47b3ca2b098a5935db378c060c05e63
Caique0Araujo <caique.ara19@gmail.com>
2023-02-19 20:37:16 -0300

    add bd creation script

---
+++ script.sql
@@ -0,0 +1,11 @@
+-- Active: 1667399386701@@127.0.0.1@3306@apae
+CREATE DATABASE apicrud;
+USE apicrud;
+
+CREATE TABLE users(
+    id_user INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    login VARCHAR(20) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    is_enabled TINYINT(1) NOT NULL DEFAULT 1
+);
