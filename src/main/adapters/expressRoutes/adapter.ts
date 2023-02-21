47cf79e60df01eb7336b3e9c702866b2fc27319d
Caique0Araujo <caique.ara19@gmail.com>
2023-02-21 11:39:49 -0300

    minor fixes

--- src/main/adapters/expressRoutes/adapter.ts
+++ src/main/adapters/expressRoutes/adapter.ts
@@ -1,15 +1,11 @@
 import { Response, Request } from "express";
 import { Controller } from "../../../presentation/controllers/controller";
 
-function isEmptyObject(obj) {
-    return !Object.keys(obj).length;
-  }
 
 export const adaptRoute = (controller: Controller) =>{
         return async (req: Request, res: Response) =>{
             const authHeader = req.headers.authorization;
             const data = {content: '', token: '', file: null}
-            data.file = req.files ? req.files.file : null;
             data.content = Object.keys(req.body).length ? req.body : req.params
             data.token = authHeader ? authHeader : null
             const httpResponse = await controller.handle(data);
--- src/main/middlewares/expressMiddlewares.ts
+++ src/main/middlewares/expressMiddlewares.ts
@@ -1,9 +1,8 @@
-import express from 'express';
 import { IMiddlewares } from "./middlewares";
 import { expressMiddlewares } from '../config/expressMiddlewares';
 
 export class ExpressMiddlewares implements IMiddlewares{
-  setMiddlewares(app: express) {
+  setMiddlewares(app) {
     expressMiddlewares.map(middleware => {
       app.use(middleware.middleware)
     })
